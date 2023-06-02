045604fdb70796aa2ceb50a535b18b88a2b011b8
Ruslan Dunec <dunecruslan2@gmail.com>
2023-06-02 14:40:40 +0300

    створив конвертер валют

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
+
+function App() {
+  const [fromCurrency, setFromCurrency] = React.useState("UAH");
+  const [toCurrency, setToCurrency] = React.useState("USD");
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(1);
+
+  const ratesRef = React.useRef({});
+
+  React.useEffect(() => {
+    fetch("https://www.cbr-xml-daily.ru/latest.js")
+      .then((res) => res.json())
+      .then((json) => {
+        ratesRef.current = json.rates;
+        onChangeToPrice(1);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("не вдалось отримати ынформацыю");
+      });
+  }, []);
+
+  const onChangeFormPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setToPrice(result.toFixed(3));
+    setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+    const result =
+      (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(3));
+    setToPrice(value);
+  };
+
+  //цей юзефект слідкує за значанням валюди в лівому блоці, щоб працювало перемикання між валютами
+  React.useEffect(() => {
+    onChangeFormPrice(fromPrice);
+  }, [fromCurrency]);
+
+  //те саме для правого блоку
+  React.useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
+
+  return (
+    <div className="App">
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFormPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
+    </div>
+  );
+}
+
+export default App;
